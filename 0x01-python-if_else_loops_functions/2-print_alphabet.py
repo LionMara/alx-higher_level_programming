59d326a7f6ee4014836fab94a8976c58a2cc0315
LionMara <lionofmara@gmail.com>
2022-09-22 16:06:27 +0300

    This is easy. Python: Hold my beer

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alpha_letters in range(97, 123):
-    print(chr(alpha_letters), end = '')
+for alpha_letters in "abcdefghijklmnopqrstuvwxyz":
+    print(f"{alpha_letters}", end = '')
