df54a1416019b568586734b5816b7fd453013d31
LionMara <lionofmara@gmail.com>
2022-09-26 21:15:45 +0300

    Trial

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        tuple1 = len(sentence), None,
+        tuple1 = 0, None,
         return tuple1
     tuple1 = len(sentence), sentence[0]
     return tuple1
