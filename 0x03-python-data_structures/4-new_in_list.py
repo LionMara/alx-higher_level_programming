f18272e9a59bf4ea99974e55beab52d060592de7
LionMara <lionofmara@gmail.com>
2022-09-26 19:57:40 +0300

    Sometimes, crazy won't kill

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    a = my_list[:]
+    if idx < 0:
+        return a
+    elif idx > len(my_list):
+        return a
+    else:
+        a[idx] = element
+
+    return a
