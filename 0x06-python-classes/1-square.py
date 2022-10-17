c44c6d2b466bacbb308b42b4fa732a65a93ba1e8
LionMara <lionofmara@gmail.com>
2022-10-17 17:20:37 +0300

    1 More class at a time

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Class Square
+the class makes a square with private instance size
+
+"""
+
+
+class Square:
+    """ A class that defines square by size """
+
+    def __init__(self, size):
+        self.__size = size
