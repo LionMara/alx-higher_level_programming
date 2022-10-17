b7fd3791d12ff3d1adabf49792cfc46001961870
LionMara <lionofmara@gmail.com>
2022-10-17 17:10:37 +0300

    Python documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""
+This is the "Square" class
+
+Creates an empty class
+"""
+
 
 class Square:
+
+    """
+    a simple class
+    Attributes:
+          No attributes
+    """
     pass
