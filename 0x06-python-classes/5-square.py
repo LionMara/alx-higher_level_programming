67d8ab1405b6b2ed28871ac12474caa27d752648
LionMara <lionofmara@gmail.com>
2022-10-19 18:41:02 +0300

    Classes are somehow making sense

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+Class Square defines a square based on 4-square.py
+The class has a property and a property setter
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            print("#"*self.__size)
