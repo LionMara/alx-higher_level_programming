175c681c933e7e33301fcd014d27f569f97f18be
LionMara <lionofmara@gmail.com>
2022-11-02 10:05:50 +0300

    Checker knows

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" json module - JavaScript oBJECT nOTATION """
+
+import json
+
+"""
+save_to_json_file: function tha writes an object
+to file using JSON rep'n
+
+"""
+
+def save_to_json_file(my_obj, filename):
+    json_obj = json.dumps(my_obj)
+
+    with open(filename, mode="a", encoding="utf-8") as my_file:
+        my_file.write(json_obj)
+
