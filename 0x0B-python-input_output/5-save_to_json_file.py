bcf47177c5f5c70bac693686a2c2ec8c87e33625
LionMara <lionofmara@gmail.com>
2022-11-02 10:18:44 +0300

    style demands

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,6 +11,8 @@ to file using JSON rep'n
 
 
 def save_to_json_file(my_obj, filename):
+    """ adds json objects to json file """
+
     json_obj = json.dumps(my_obj)
 
     with open(filename, mode="a", encoding="utf-8") as my_file:
