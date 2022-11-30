69eb076bd8511b0499dae5bded52f6dccb11a4f8
LionMara <lionofmara@gmail.com>
2022-11-30 22:11:06 +0300

    Man, it is like titration

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,4 +3,4 @@
 -- database name will be passed as an argument of mysql command
 -- no use SELECT or SHOW
 
-CREATE TABLE [IF NOT EXISTS]  first_table(id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS  first_table (id INT, name VARCHAR(256));
