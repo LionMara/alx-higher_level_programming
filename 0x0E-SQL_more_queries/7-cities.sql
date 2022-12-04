6d44dbe493b8304682a7f23738fa746bae3874e0
LionMara <lionofmara@gmail.com>
2022-12-04 15:52:33 +0300

    Debugging MySql is not fun

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- a script that creates db -hbtn_0d_usa table - states
+-- states table
+-- (id-INT) auto-generated, no null, primary key
+-- name-VARCHAR(256) NOT NULL
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+       (id INT UNIQUE NOT NULL AUTO_INCREMENT,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY(id),
+       FOREIGN KEY(state_id)
+       REFERENCES states(id));
