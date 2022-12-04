6fafb4f886b74bc9fc70fe60bbc059e91083909a
LionMara <lionofmara@gmail.com>
2022-12-04 15:19:34 +0300

    Well, three not same as four

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- a script that creates the table id_not_null
+-- id_not_null -> id-INT, name-VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS id_not_null
+       (id INT, name VARCHAR(256) NOT NULL);
