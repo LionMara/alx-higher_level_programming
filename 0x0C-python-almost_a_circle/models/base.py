d2d2fb4349fcecc878291c47d40c52ff81f25604
LionMara <lionofmara@gmail.com>
2023-01-29 20:26:50 +0300

    Comment me

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,9 +7,13 @@ a bunch of stuff
 '''
 
 class Base():
+    ''' This is the base of the project
+        all the functions will stay here
+    '''
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''constructor for the base class '''
         if id:
             self.id = id
         else:
