c83ed57fb798bab2156fffe03bc70299e9cfedd1
LionMara <lionofmara@gmail.com>
2023-01-31 12:51:22 +0300

    Style

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,7 @@
 
 ''' using __slot__ '''
 
+
 class LockedClass():
     '''class or object attribute,
        that prevents the user from dynamically
@@ -9,4 +10,5 @@ class LockedClass():
        except if the new instance attribute
        is called first_name
     '''
+
     __slots__ = ['first_name']
