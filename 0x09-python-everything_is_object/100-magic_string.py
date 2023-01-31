62c52161421d0b111b9e149aaa33a1b5fc8248a9
LionMara <lionofmara@gmail.com>
2023-01-31 12:54:15 +0300

    The requirements

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 def magic_string():
-    '''returns BestSchool n times'''
-
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return (", BestSchool" * magic_string.n)[2:]
