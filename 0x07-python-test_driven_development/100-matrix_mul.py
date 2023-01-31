b7442e92095b3be3d4cae1d8dc865feb6d8dd368
LionMara <lionofmara@gmail.com>
2023-01-31 19:20:50 +0300

    Tweaks for 100*

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -21,7 +21,7 @@ def matrix_mul(m_a, m_b):
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
 
-    if m_b == [] or m_a == [[]]:
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
     if not all((isinstance(ele, int) or isinstance(ele, float))
@@ -33,10 +33,10 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
 
-    if not all(len(row) == len(m_a[0]) for row in m_b):
-        raise TypeError("each row of m_a must should be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
