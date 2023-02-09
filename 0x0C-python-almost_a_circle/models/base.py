79600e1879dacdbe3f087db3f7c5413baca68b96
LionMara <lionofmara@gmail.com>
2023-02-09 09:48:16 +0300

    JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+'''moduls that are imported'''
+import json
 
 '''
 Class Base it will do
@@ -20,3 +22,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        ''' returns JSON represenation of list_dictionaries '''
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
