3fb7c8b22aaa19541391c190d6cb7c29541efbf8
LionMara <lionofmara@gmail.com>
2023-03-13 22:49:20 +0300

    gang gang

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+let i = 0;
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i < x) {
+    console.log('C is Fun');
+    i++;
+  }
+}
