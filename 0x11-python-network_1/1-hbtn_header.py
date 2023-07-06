2180c91d441abab9fa53779589f18c114ced3867
LionMara <lionofmara@gmail.com>
2023-07-06 09:37:14 +0300

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-     - script that takes in a URL
-     - sends a request to the URL
-     - displays the value of the X-Request-Id variable
+- script that takes in a URL
+- sends a request to the URL
+- displays the value of the X-Request-Id variable
 """
 
 import urllib.request
 import sys
 
 if __name__ == "__main__":
+    """ prevents module from running if not called directly """
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         headers = response.headers
